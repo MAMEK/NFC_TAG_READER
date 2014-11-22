ac9544b6a8bffd507a1569e1f770305fb43a22ff
neur0genesis <mskm14@student.aau.dk>
2014-11-22 17:18:35 +0100

    Changelog: Implemted forground dispatch and intent filters

--- app/src/main/java/com/example/neur0genesis/nfc_tag_reader/MainActivity.java
+++ app/src/main/java/com/example/neur0genesis/nfc_tag_reader/MainActivity.java
@@ -8,6 +8,12 @@ import android.nfc.NfcAdapter;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.IntentFilter.MalformedMimeTypeException;
+
+
 
 /*
 * Activity for reading data from an NDEF Tag.
@@ -19,6 +25,8 @@ public class MainActivity extends Activity {
 
     private TextView mTextView; //Object for displaying text to the user. (Can be edited but optional).
     private NfcAdapter mNfcAdapter; //Object for fetching the default NFC adapter by using a helper.
+
+    public static final String MIME_TEXT_PLAIN = "text/plain";
 
     @Override //Annotation to indicate that we are overriding the superclass.
     protected void onCreate(Bundle savedInstanceState) { //'Protected' limits access to the package.
@@ -45,8 +53,76 @@ public class MainActivity extends Activity {
 
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
 
+        /**
+         * It's important, that the activity is in the foreground (resumed). Otherwise
+         * an IllegalStateException is thrown.
+         */
+        setupForegroundDispatch(this, mNfcAdapter);
+    }
 
+    @Override
+    protected void onPause() {
+        /**
+         * Call this before onPause, otherwise an IllegalArgumentException is thrown as well.
+         */
+        stopForegroundDispatch(this, mNfcAdapter);
+
+        super.onPause();
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        /**
+         * This method gets called, when a new Intent gets associated with the current activity instance.
+         * Instead of creating a new activity, onNewIntent will be called. For more information have a look
+         * at the documentation.
+         *
+         * In our case this method gets called, when the user attaches a Tag to the device.
+         */
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+        // TODO: handle Intent
+    }
+
+    /**
+     * @param activity The corresponding {@link Activity} requesting the foreground dispatch.
+     * @param adapter The {@link NfcAdapter} used for the foreground dispatch.
+     */
+    public static void setupForegroundDispatch(final Activity activity, NfcAdapter adapter) {
+        final Intent intent = new Intent(activity.getApplicationContext(), activity.getClass());
+        intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+        final PendingIntent pendingIntent = PendingIntent.getActivity(activity.getApplicationContext(), 0, intent, 0);
+
+        IntentFilter[] filters = new IntentFilter[1];
+        String[][] techList = new String[][]{};
+
+        // Notice that this is the same filter as in our manifest.
+        filters[0] = new IntentFilter();
+        filters[0].addAction(NfcAdapter.ACTION_NDEF_DISCOVERED);
+        filters[0].addCategory(Intent.CATEGORY_DEFAULT);
+        try {
+            filters[0].addDataType(MIME_TEXT_PLAIN);
+        } catch (MalformedMimeTypeException e) {
+            throw new RuntimeException("Check your mime type.");
+        }
+
+        adapter.enableForegroundDispatch(activity, pendingIntent, filters, techList);
+    }
+
+    /**
+     * @param activity The corresponding {@link MainActivity} requesting to stop the foreground dispatch.
+     * @param adapter The {@link NfcAdapter} used for the foreground dispatch.
+     */
+    public static void stopForegroundDispatch(final Activity activity, NfcAdapter adapter) {
+        adapter.disableForegroundDispatch(activity);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
